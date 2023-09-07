1b23e764d5abd7dd4c638ff49471729b934720be
chikkka <141250393+chikkka@users.noreply.github.com>
2023-09-07 13:08:33 +0900

    bmi.jsをアップロード

--- javascript-basic-kadai/kadi-005/bmi.js
+++ javascript-basic-kadai/kadi-005/bmi.js
@@ -4,17 +4,16 @@ let height='身長';
 let bmi='体格指数';
 
 //コンソールへの出力
-console.log('bodyWeight');
-console.log('height');
-console.log('bmi');
+console.log(bodyWeight);
+console.log(height);
+console.log(bmi);
 
 //値の再代入
 bodyWeight=68;
 height=1.7;
-bmi=23.5294118;
+bmi=(bodyWeight/(height*height));
 
 //コンソールへの出力
-console.log('bodyWeight');
-console.log('height');
-console.log('bmi');
-console.log(68/(1.7*1.7));
+console.log(bodyWeight);
+console.log(height);
+console.log(bmi);
